9c8bf019819fdf2bc92f2ec38ab3b1dbca3ba35c
Elijah Ford <edfor176@learner.dvusd.org>
2022-08-17 08:46:21 -0700

    Wednesday

---
+++ C#/Assignment/Assignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int correct = 0;
+           
+          
+
+            correct = correct + 1;
+            Console.WriteLine("How many times days can Elijah sleep in mr Clarkes class in a week");
+            Console.WriteLine("A. Everyday");
+            Console.WriteLine("B. 2");
+            Console.WriteLine("C. none");
+            Console.WriteLine("D. 3");
+          string answer = Console.ReadLine();
+            if (answer == "A")
+                Console.WriteLine("whats 4 times 4");
+            Console.WriteLine("A. 16");
+            Console.WriteLine("B. 2");
+            Console.WriteLine("C. 0");
+            Console.WriteLine("D. 3");
+              answer = Console.ReadLine(); 
+               if (answer == "A")
+            Console.WriteLine("Are you exuasted");
+            Console.WriteLine("A. No I dont get tired");
+            Console.WriteLine("B. Yes");
+            Console.WriteLine("C. Im depressed ");
+            Console.WriteLine("D. I need help");
+            answer = Console.ReadLine();
+            if (answer == "A")
+                Console.WriteLine("you got  right "+correct);
+                Console.ReadLine();
+            
+
+
+
+
+
+        }
+    }
+}
---
+++ C#/Survey/Survey/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("enter your age");
+            x=int.Parse(Console.ReadLine());
+
+            Console.WriteLine(x);
+            Console.WriteLine("your age is " + x);
+            Console.WriteLine("your name is {0}.",x);
+            Console.WriteLine(@"your age is{x} ");
+            Console.ReadKey();
+           
+
+           
+
+
+
+
+
+        }
+    }
+}
